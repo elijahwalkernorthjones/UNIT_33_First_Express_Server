3755c54ec9e0055a538cb3bbc809e472c802a14d
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-02-03 18:01:51 -0500

    more practice using express to create routes for server on localhost 3000

--- expressRoutes.js
+++ expressRoutes.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', function(req, res){
+    res.send("home page")
+})
 
 app.get('/dogs', function(req, res) {
     console.log("dogssssss")
-    console.log(req)
     res.send("WOOOOOOF!!!")
+})
+
+app.get('/dogs', function(req, res) {
+    console.log("dogssssss")
+    res.send("MEOWMEOWMEOWWWWW!!!")
 })
 
 
