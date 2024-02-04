40d4a054953ca25f0e05b0c58945c19caf3349eb
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-02-04 13:43:59 -0500

    added usernames using a submit form and JSON with a register post request

--- expressRoutes.js
+++ expressRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()) // this is used if we want to enable the use of JSON
+app.use(express.urlencoded({encoded: true})); // if we want to enable form data
+
 //------------------------------------------
 
 
@@ -67,9 +70,15 @@ app.get('/show-language', function(req, res){
      res.send(`Your language preference is ${lang}`)
 })
 
+
+
 //---------------------------------------------
 
+/** parsing with req.body */
 
+app.post('/register', function(req,res){
+    res.send(`Welcome, ${req.body.username}!`);
+})
 
 
 
