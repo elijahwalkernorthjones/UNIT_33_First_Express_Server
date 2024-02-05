291940e63f63434651ef388b692cc0a1af33e54e
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-02-05 09:39:20 -0500

    status codes

--- jsonApi.js
+++ jsonApi.js
@@ -5,11 +5,15 @@ const app = express();
 app.use(express.json()) // this is used if we want to enable the use of JSON
 
 
+/** teensy database */
+
 const CANDIES = [
     {name: 'snickers', qty: 43, price: 1.50},
     {name: 'skittles', qty: 26, price: .99}
 ]
 
+
+/** middleware & routes */
 
 app.get("/candies", function(req, res) {
     res.json(CANDIES);
@@ -17,7 +21,7 @@ app.get("/candies", function(req, res) {
 
 app.post('/candies', function(req,res) {
     CANDIES.push(req.body);
-    res.json(CANDIES);
+    res.status(201).json(CANDIES); // status 201 becuase something is being created
 })
 
 
@@ -29,7 +33,7 @@ app.post('/candies', function(req,res) {
 
 
 
-
+/** app.listen */
 app.listen(3000, () => {
     console.log("Server running on port 3000")
 });
