d65eaba4f39536198e6aef45d9e4f7c3e69ae816
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-02-05 09:16:36 -0500

    made some get and post requests with JSON

--- jsonApi.js
+++ jsonApi.js
@@ -3,6 +3,33 @@ const express = require('express');
 const app = express();
 
 app.use(express.json()) // this is used if we want to enable the use of JSON
-app.use(express.urlencoded({encoded: true})); // if we want to enable form data
 
 
+const CANDIES = [
+    {name: 'snickers', qty: 43, price: 1.50},
+    {name: 'skittles', qty: 26, price: .99}
+]
+
+
+app.get("/candies", function(req, res) {
+    res.json(CANDIES);
+})
+
+app.post('/candies', function(req,res) {
+    CANDIES.push(req.body);
+    res.json(CANDIES);
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
+});
